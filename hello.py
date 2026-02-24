05c40bcf617f35dde2b242dc1249eb039c96b785
UB15-LAB-004 <ub15-lab-004@chanakyauni.local>
2026-02-24 16:49:24 +0530

    change code

--- hello.py
+++ hello.py
@@ -1,6 +1,31 @@
-i=1
+"""i=1
 while i<=10:
     print(i)
     i+=1
 if num % 2 !=0:
     print(num)
+    if num % 5==0:
+        print)(num)
+ int(input("enter number"))
+       if marks >=90:
+       print)(A)
+int(input("emter number")
+    if marks >=75:
+    print)(B)
+int(int)
+
+
+"""
+while True:
+    a=int(input("Enter marks:"))
+
+    if a==-1:
+        break
+    elif a>=90:
+        print("A")
+    elif a>=75:
+        print("B")
+    elif a>=50:
+        print("C")
+    else:
+        print("F")
